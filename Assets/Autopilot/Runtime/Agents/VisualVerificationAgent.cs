17d91f20af5f9b870c51cf33b501093f4c912d8f
Koji Hasegawa <hasegawa@hubsys.co.jp>
2025-06-30 17:24:51 +0900

    Fix and rename to VisualVerificationAgent

--- Assets/Autopilot/Runtime/Agents/VisualVerificationAgent.cs
+++ Assets/Autopilot/Runtime/Agents/VisualVerificationAgent.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using DeNA.Anjin;
@@ -10,18 +11,22 @@ using UnityEngine;
 
 namespace Autopilot.Agents
 {
-    [CreateAssetMenu(fileName = "New JudgeByGeminiAgent", menuName = "Anjin/GalacticKittens/Judge by Gemini Agent",
-        order = 42)]
-    public class JudgeByGeminiAgent : AbstractAgent
+    /// <summary>
+    /// Visual Verification Agent using Google Gemini AI.
+    /// </summary>
+    /// <see href="https://github.com/nuskey8/GemiNet"/>
+    /// <see href="https://ai.google.dev/gemini-api/docs/image-understanding"/>
+    [CreateAssetMenu(fileName = "New VisualVerificationAgent",
+        menuName = "Anjin/GalacticKittens/Visual Verification Agent", order = 42)]
+    public class VisualVerificationAgent : AbstractAgent
     {
-        [Tooltip("処理開始までの遅延（秒）")]
+        [Tooltip("処理開始までの遅延時間（秒）")]
         public int delay;
 
-        [Tooltip("Geminiに渡すプロンプト")]
-        [Multiline]
+        [Tooltip("判定基準（Geminiに渡すプロンプト）")]
         public string prompt;
 
-        [Tooltip("成功とみなすスコアの閾値")]
+        [Tooltip("成功とみなすスコアの閾値（Max 1.0）")]
         public float threshold = 0.8f;
 
         public override async UniTask Run(CancellationToken cancellationToken)
@@ -41,19 +46,33 @@ namespace Autopilot.Agents
                     return;
                 }
 
-                await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate);
-                var texture = ScreenCapture.CaptureScreenshotAsTexture(); // TODO: asyncにすべき（Anjinに実装したい）
+                var coroutineRunner = new GameObject().AddComponent<CoroutineRunner>();
+                await UniTask.WaitForEndOfFrame(coroutineRunner, cancellationToken);
+                Destroy(coroutineRunner);
+                // Note: UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate)では解像度を大きくしたらダメだった
+
+                var texture = ScreenCapture.CaptureScreenshotAsTexture();
                 var bytes = texture.EncodeToPNG();
                 var base64 = Convert.ToBase64String(bytes);
-                // Note: 画像サイズは20MB制限があるので、GameViewの解像度を下げておくこと！
+                // Note: GameViewの解像度が低いとLLMが読み取れないので注意
 
                 using var ai = new GoogleGenAI();
                 ai.ApiKey = apikey.Value();
+
+                var file = await ai.Files.UploadAsync(
+                    new UploadFileRequest()
+                    {
+                        File = new UploadFileContent(new Blob() { Data = base64, MimeType = "image/png" }),
+                        MimeType = "image/png",
+                    },
+                    cancellationToken: cancellationToken);
+                Logger.Log($"Upload screenshot to {file.Uri}");
+
                 var response = await ai.Models.GenerateContentAsync(new GenerateContentRequest
                     {
                         Model = Models.Gemini2_0Flash, // models/gemini-2.0-flash
                         Contents = Content.CreateUserContent(
-                            Part.FromBase64(base64, "image/png"),
+                            Part.FromUri(file.Uri, file.MimeType),
                             Part.FromText(prompt)
                         ),
                         SystemInstruction =
@@ -74,16 +93,31 @@ namespace Autopilot.Agents
                         }
                     },
                     cancellationToken: cancellationToken);
+                Logger.Log($"Response: {response.GetText()}\n{response}");
 
-                Logger.Log($"Response: {response}");
-                Logger.Log($"Response.text: {response.GetText()}");
-
-                // TODO: thresholdを超えないときは失敗させる
+                var json = JsonDocument.Parse(response.GetText()).RootElement;
+                var score = json.GetProperty("score").GetDouble();
+                var comment = json.GetProperty("comment").GetString();
+                if (score < threshold)
+                {
+                    var message = $"Failure! score:{score} comment:{comment}";
+                    Logger.Log(message);
+                    AutopilotInstance.TerminateAsync(ExitCode.AutopilotFailed, message).Forget();
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Log(e.ToString());
+                AutopilotInstance.TerminateAsync(ExitCode.AutopilotFailed, e.ToString(), e.StackTrace).Forget();
             }
             finally
             {
                 Logger.Log($"Exit {this.name}.Run()");
             }
+        }
+
+        private class CoroutineRunner : MonoBehaviour
+        {
         }
     }
 }
